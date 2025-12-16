2b8e2774adbcc125ef7c93b8ae3848fd51b3751c
Joao Carmassi <joaovitorcarmassi@gmail.com>
2025-12-16 16:40:19 -0300

    fix menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -29,24 +29,11 @@ import { cn } from '@/lib/utils';
 import shuffleArray from '@/utils/shuffle-array';
 import itemsPerCategory from '@/utils/items-per-category';
 import InputEscondeProdutos from './InputEscondeNav';
-import { MessageSquareText, SearchIcon } from 'lucide-react';
+import { SearchIcon } from 'lucide-react';
 import { ButtonGroup } from './ui/button-group';
 import { Input } from './ui/input';
 import MenuContato from './menu-contato';
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from './ui/sheet';
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from './ui/accordion';
-import { Separator } from './ui/separator';
+import MenuMobile from './menu-mobile';
 
 const navItens = [
   {
@@ -94,132 +81,8 @@ export default function Header(): React.ReactNode {
         <BannerPix />
         <div className='flex h-16 items-center justify-between gap-4 max-w-[95rem] mx-auto px-6 lg:px-12'>
           <div className='flex-1 flex items-center lg:justify-end gap-3'>
-            {/* Mobile menu trigger */}
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button
-                  className='group size-10 lg:hidden text-card border border-card'
-                  variant='ghost'
-                  size='icon'
-                >
-                  <svg
-                    className='pointer-events-none'
-                    width={16}
-                    height={16}
-                    viewBox='0 0 24 24'
-                    fill='none'
-                    stroke='currentColor'
-                    strokeWidth='2'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      d='M4 12L20 12'
-                      className='origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]'
-                    />
-                    <path
-                      d='M4 12H20'
-                      className='origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45'
-                    />
-                    <path
-                      d='M4 12H20'
-                      className='origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]'
-                    />
-                  </svg>
-                </Button>
-              </SheetTrigger>
-              <SheetContent side='left' className='overflow-y-auto'>
-                <SheetHeader>
-                  <SheetTitle hidden>Menu</SheetTitle>
-                  <div className='flex flex-col gap-5 text-sm'>
-                    <Link href='/' className='flex gap-2 items-center'>
-                      <Image
-                        src='/imgs/logos/favicon.ico'
-                        alt='Logo'
-                        width={25}
-                        height={25}
-                        className='border-2 border-secondary p-1 rounded-full aspect-square'
-                      />
-                      <p className='font-bold text-primary text-lg'>
-                        Loja do VRF
-                      </p>
-                    </Link>
-                    <Separator />
-                    <div className='flex flex-col gap-3'>
-                      <Link
-                        className='flex items-center gap-2'
-                        href='/manuais-tecnicos'
-                      >
-                        <Image
-                          src='/imgs/nav/logoManualCelular.webp'
-                          alt='Manuais Técnicos'
-                          width={15}
-                          height={15}
-                        />
-                        Manuais Técnicos
-                      </Link>
-                      <Link
-                        className='flex items-center gap-2'
-                        href='/manuais-tecnicos'
-                      >
-                        <MessageSquareText
-                          strokeWidth={1.5}
-                          size={18}
-                          className='text-[#004fa2]'
-                        />
-                        Central de Atendimento
-                      </Link>
-                    </div>
-                    <Separator />
-                    <div className='flex flex-col gap-3'>
-                      <h3 className='font-semibold'>Categorias:</h3>
-                      {categorias.map((categoria, i) => {
-                        const currentCategoria =
-                          itens[categoria as keyof typeof itens];
-                        return currentCategoria.subcategorias.length === 0 ? (
-                          <Link
-                            key={categoria + i}
-                            href={`/produtos/${generateUrl(categoria)}`}
-                            className='capitalize'
-                          >
-                            {categoria}
-                          </Link>
-                        ) : (
-                          <Accordion
-                            key={categoria + i}
-                            type='single'
-                            collapsible
-                          >
-                            <AccordionItem value='item-1'>
-                              <AccordionTrigger className='py-0 capitalize'>
-                                {categoria}
-                              </AccordionTrigger>
-                              <AccordionContent className='pt-3 flex flex-col gap-3'>
-                                {currentCategoria.subcategorias.map(
-                                  (subcat, i) => (
-                                    <Link
-                                      key={subcat + i}
-                                      href={`/produtos/${generateUrl(
-                                        categoria
-                                      )}?subcategoria=${generateUrl(subcat)}`}
-                                      className='capitalize'
-                                    >
-                                      {subcat}
-                                    </Link>
-                                  )
-                                )}
-                                <Separator />
-                              </AccordionContent>
-                            </AccordionItem>
-                          </Accordion>
-                        );
-                      })}
-                    </div>
-                  </div>
-                </SheetHeader>
-              </SheetContent>
-            </Sheet>
+            {/* Mobile menu */}
+            <MenuMobile />
             <Link className='flex gap-3 items-center' href='/'>
               <Image
                 src='/imgs/logos/favicon.ico'
@@ -248,13 +111,15 @@ export default function Header(): React.ReactNode {
                 <SearchIcon />
               </Button>
             </ButtonGroup>
-            <Image
-              src='/imgs/logos/logo.webp'
-              alt='Loja do VRF'
-              width={500}
-              height={70}
-              className='h-full mx-auto lg:hidden'
-            />
+            <Link href='/'>
+              <Image
+                src='/imgs/logos/logo.webp'
+                alt='Loja do VRF'
+                width={500}
+                height={60}
+                className='h-full mx-auto lg:hidden'
+              />
+            </Link>
           </div>
           <div className='flex-1 flex justify-end lg:justify-start gap-3'>
             <DropdownMenu>
---
+++ src/components/menu-mobile.tsx
@@ -0,0 +1,147 @@
+import getProdutos from '@/utils/getProdutos';
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet';
+import { Button } from './ui/button';
+import generateUrl from '@/utils/generateUrl';
+import Image from 'next/image';
+import { Separator } from './ui/separator';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from './ui/accordion';
+import Link from 'next/link';
+import { MessageSquareText } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+
+function MenuMobile(): React.ReactNode {
+  const { categorias, subcategorias: itens } = getProdutos;
+  const pathname = usePathname();
+
+  return (
+    <Sheet key={pathname}>
+      <SheetTrigger asChild>
+        <Button
+          className='group size-10 lg:hidden text-card border border-card'
+          variant='ghost'
+          size='icon'
+        >
+          <svg
+            className='pointer-events-none'
+            width={16}
+            height={16}
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M4 12L20 12'
+              className='origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]'
+            />
+            <path
+              d='M4 12H20'
+              className='origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45'
+            />
+            <path
+              d='M4 12H20'
+              className='origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]'
+            />
+          </svg>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side='left' className='overflow-y-auto'>
+        <SheetHeader>
+          <SheetTitle hidden>Menu</SheetTitle>
+          <div className='flex flex-col gap-5 text-sm'>
+            <Link href='/' className='flex gap-2 items-center'>
+              <Image
+                src='/imgs/logos/favicon.ico'
+                alt='Logo'
+                width={25}
+                height={25}
+                className='border-2 border-secondary p-1 rounded-full aspect-square'
+              />
+              <p className='font-bold text-primary text-lg'>Loja do VRF</p>
+            </Link>
+            <Separator />
+            <div className='flex flex-col gap-3'>
+              <Link
+                className='flex items-center gap-2'
+                href='/manuais-tecnicos'
+              >
+                <Image
+                  src='/imgs/nav/logoManualCelular.webp'
+                  alt='Manuais Técnicos'
+                  width={15}
+                  height={15}
+                />
+                Manuais Técnicos
+              </Link>
+              <Link
+                className='flex items-center gap-2'
+                href='/manuais-tecnicos'
+              >
+                <MessageSquareText
+                  strokeWidth={1.5}
+                  size={18}
+                  className='text-[#004fa2]'
+                />
+                Central de Atendimento
+              </Link>
+            </div>
+            <Separator />
+            <div className='flex flex-col gap-3'>
+              <h3 className='font-semibold'>Categorias:</h3>
+              {categorias.map((categoria, i) => {
+                const currentCategoria = itens[categoria as keyof typeof itens];
+                return currentCategoria.subcategorias.length === 0 ? (
+                  <Link
+                    key={categoria + i}
+                    href={`/produtos/${generateUrl(categoria)}`}
+                    className='capitalize'
+                  >
+                    {categoria}
+                  </Link>
+                ) : (
+                  <Accordion key={categoria + i} type='single' collapsible>
+                    <AccordionItem value='item-1'>
+                      <AccordionTrigger className='py-0 capitalize'>
+                        {categoria}
+                      </AccordionTrigger>
+                      <AccordionContent className='pt-3 flex flex-col gap-3'>
+                        {currentCategoria.subcategorias.map((subcat, i) => (
+                          <Link
+                            key={subcat + i}
+                            href={`/produtos/${generateUrl(
+                              categoria
+                            )}?subcategoria=${generateUrl(subcat)}`}
+                            className='capitalize'
+                          >
+                            {subcat}
+                          </Link>
+                        ))}
+                        <Separator />
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                );
+              })}
+            </div>
+          </div>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default MenuMobile;
