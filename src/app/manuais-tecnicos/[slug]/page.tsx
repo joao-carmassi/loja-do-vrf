05b839b5dc00c294f236f3b1059d3c66cab91cdd
Joao Carmassi <joaovitorcarmassi@gmail.com>
2026-01-08 21:27:58 -0300

    pequenos ajustes

--- src/app/manuais-tecnicos/[slug]/page.tsx
+++ src/app/manuais-tecnicos/[slug]/page.tsx
@@ -136,52 +136,52 @@ const Manuais = async ({ params }: Props): Promise<React.ReactNode> => {
           <h1 className='w-full text-center text-2xl font-medium md:text-3xl text-primary'>
             Manuais Técnicos - {marcaNome}
           </h1>
-          {listaPdfs.some((pdf) => pdf.tipo === 'CONDENSADORA') && (
-            <div className='flex flex-col gap-7'>
-              <h2 className='text-xl uppercase'>/ Unidade Condensadora</h2>
-              <div>
-                {listaPdfs
-                  .filter((pdf) => pdf.tipo === 'CONDENSADORA')
-                  .map((pdf, idx) => (
-                    <div key={idx}>
-                      <Separator />
-                      <a
-                        href={`/pdfs/${pdf.documento}.pdf`}
-                        download={`${pdf.documento}.pdf`}
-                        className='my-2.5 grid gap-2.5 text-sm sm:grid-cols-3 hover:underline'
-                      >
-                        <p className='text-muted-foreground'>{pdf.marca}</p>
-                        <p>{pdf.documento}</p>
-                        <p className='text-muted-foreground'>{pdf.modelo}</p>
-                      </a>
-                    </div>
-                  ))}
+          <div className='grid md:grid-cols-2 gap-12'>
+            {listaPdfs.some((pdf) => pdf.tipo === 'CONDENSADORA') && (
+              <div className='flex flex-col gap-7'>
+                <h2 className='text-xl uppercase'>/ Unidade Condensadora</h2>
+                <div>
+                  {listaPdfs
+                    .filter((pdf) => pdf.tipo === 'CONDENSADORA')
+                    .map((pdf, idx) => (
+                      <div key={idx}>
+                        <Separator />
+                        <a
+                          href={`/pdfs/${pdf.documento}.pdf`}
+                          download={`${pdf.documento}.pdf`}
+                          className='my-2.5 grid gap-2.5 text-sm grid-cols-2 hover:underline'
+                        >
+                          <p>{pdf.documento}</p>
+                          <p className='text-muted-foreground'>{pdf.modelo}</p>
+                        </a>
+                      </div>
+                    ))}
+                </div>
               </div>
-            </div>
-          )}
-          {listaPdfs.some((pdf) => pdf.tipo === 'EVAPORADORA') && (
-            <div className='flex flex-col gap-7'>
-              <h2 className='text-xl uppercase'>/ Unidade Evaporadora</h2>
-              <div>
-                {listaPdfs
-                  .filter((pdf) => pdf.tipo === 'EVAPORADORA')
-                  .map((pdf, idx) => (
-                    <div key={idx}>
-                      <Separator />
-                      <a
-                        href={`/pdfs/${pdf.documento}.pdf`}
-                        download={`${pdf.documento}.pdf`}
-                        className='my-2.5 grid gap-2.5 text-sm sm:grid-cols-3 hover:underline'
-                      >
-                        <p className='text-muted-foreground'>{pdf.marca}</p>
-                        <p>{pdf.documento}</p>
-                        <p className='text-muted-foreground'>{pdf.modelo}</p>
-                      </a>
-                    </div>
-                  ))}
+            )}
+            {listaPdfs.some((pdf) => pdf.tipo === 'EVAPORADORA') && (
+              <div className='flex flex-col gap-7'>
+                <h2 className='text-xl uppercase'>/ Unidade Evaporadora</h2>
+                <div>
+                  {listaPdfs
+                    .filter((pdf) => pdf.tipo === 'EVAPORADORA')
+                    .map((pdf, idx) => (
+                      <div key={idx}>
+                        <Separator />
+                        <a
+                          href={`/pdfs/${pdf.documento}.pdf`}
+                          download={`${pdf.documento}.pdf`}
+                          className='my-2.5 grid gap-2.5 text-sm grid-cols-2 hover:underline'
+                        >
+                          <p>{pdf.documento}</p>
+                          <p className='text-muted-foreground'>{pdf.modelo}</p>
+                        </a>
+                      </div>
+                    ))}
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </section>
       <MarcasEFotos />
